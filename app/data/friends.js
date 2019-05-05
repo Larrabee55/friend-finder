a32d4368efa3a3d25e7d2baba279080725ea3eb1
Cameron Larrabee <cameronlarrabee@yahoo.com>
2019-05-04 20:16:11 -0600

    add the array of friends/Gamers to this js file

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+// array of gamers
+var gamers = [{
+  "name": "cameron",
+  "photo": "me.jpg",
+  "scores": [
+    3,
+    4,
+    5,
+    4,
+    4,
+    5,
+    3,
+    2,
+    4,
+    1
+  ]
+}];
+// exports the array
+module.exports = gamers;
