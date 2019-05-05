c6846f3eb2babc445db589d1194f5bbd5dfe3ec9
Cameron Larrabee <cameronlarrabee@yahoo.com>
2019-05-04 20:11:29 -0600

    added the require routing

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 var express = require("express");
-var path = require("path");
+var http = require("http");
+
+var server = http.createServer();
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+// requires the routing files and sends them the variable App which equals express
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
